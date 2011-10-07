32bd4d0353b1c31fe8262543bff3e86c684ffeb6
cosmo(コスモ) <cosmo_originstring@yahoo.co.jp>
2011-10-07 09:46:24 +0900

    libnotifyの通知に対応

--- fav_unfav_tab.rb
+++ fav_unfav_tab.rb
@@ -4,6 +4,12 @@ miquire :addon, 'addon'
 miquire :mui, 'skin'
 miquire :mui, 'timeline'
 
+=begin
+libnotifyの通知を有効にするには
+$sudo apt-get install libnotify-bin
+を実行してlibnotifyをインストールしてください。
+=end
+
 Module.new do
   main = Gtk::TimeLine.new()
   image = Gtk::Image.new(Gdk::Pixbuf.new(MUI::Skin.get("fav.png"), 24,24))
@@ -15,10 +21,20 @@ Module.new do
   plugin.add_event(:favorite){ |service, fav_by, messages|
       main.add(messages)
       main.favorite(fav_by, messages)
+	  if command_exist? "notify-send"
+        SerialThread.new {
+			bg_system("notify-send","fav_by:#{fav_by}","#{messages.user.idname}:#{messages}")
+	  }
+	  end
  }
   plugin.add_event(:unfavorite){ |service, unfav_by, messages|
       main.add(messages)
       main.unfavorite(unfav_by,messages)
+	  if command_exist? "notify-send"
+        SerialThread.new {
+			bg_system("notify-send","unfav_by:#{unfav_by}","#{messages.user.idname}:#{messages}")
+	  }
+	  end
  }
-
+  
 end
